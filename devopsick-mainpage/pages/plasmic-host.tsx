649bbadece115145a78cf64c2a14c95fc811ba3a
Frikadellios <frikadellios@gmail.com>
2023-01-12 17:26:29 +0200

    Main

--- devopsick-mainpage/pages/plasmic-host.tsx
+++ devopsick-mainpage/pages/plasmic-host.tsx
@@ -4,6 +4,29 @@ import Script from 'next/script';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '../plasmic-init';
 
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
 }
